b822128e7757861c8c207be26b4abeb8f5b3a2d1
iamomarr <109823719+iamomarr@users.noreply.github.com>
2022-08-08 21:56:33 +0300

    Update rmi.java

--- rmi.java
+++ rmi.java
@@ -3,37 +3,37 @@
 //omarabuali 11923947
 
 public interface serverrmi {
-public double calculat(int ndays);
-}
-
-public class RMIserver1 implements serverrmi {
-public double calculat(int ndays) {
-return ndays * 10;
-}
-}
-
-public class RMIserver2 implements serverrmi {
-public double calculat(int ndays) {
-return ndays * 10 - ndays * 0.05;
-}
-}
-
-public class AdvertisementRMIClient {
-public static void main(String[] args) {
-int ndays = Integer.parseInt(args[0]);
-
-serverrmi server = null;
-
-try {
-if (ndays <= 10) {
-server = (serverrmi) Naming.lookup("rmi://localhost/server1");
-} else {
-server = (serverrmi) Naming.lookup("rmi://localhost/server2");
-}
-
-System.out.println("cost: " + server.calculat(ndays));
-} catch (Exception e) {
-e.printStackTrace();
-}
-}
-}
+    public double calculat(int days,int equation1,int equation2 );
+    }
+    
+    public class server1 implements serverrmi { //def first server with first equation
+    public double calculat(int days, int equation1 ) {
+        equation1 = days  * 10;
+        System.out.println(equation1);
+    }
+    }
+    
+    public class server2 implements serverrmi { //def second server with second equation
+    public double calculat(int days,int equation2) {
+        equation2 = days  * 10 - days  * 0.05;
+        System.out.println(equation2);
+    }
+    }
+    
+    public class clientclass {
+    public static void main(String[] args) {
+    int days  = Integer.parseInt(args[0]);
+    
+    serverrmi server = null;
+    
+    try{ //here we put the if stetment as u ask in the graph of hmw
+    if (days  <= 10) {
+    server =  ("//localhost/server1");
+    } else {
+    server =  Naming.lookup("//localhost/server2");
+    }
+    
+    System.out.println("cost: " + server.calculat(days));
+    }
+    }
+    }
